d3ec052dd3f444bd3e397d149a2f1b40b6f6befa
Mauri <mauri.monzon91@gmail.com>
2024-08-08 16:46:03 -0300

    Marcador y componente reutilizable

--- src/presentation/screens/maps/MapsScreen.tsx
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Map} from '../../components/maps/Map';
 
 export const MapsScreen = () => {
   return (
     <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}
-    >
-    </MapView>
-  </View>
+      <Map />
+    </View>
   );
 };
 
@@ -24,7 +14,4 @@ const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
   },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
- });
+});
