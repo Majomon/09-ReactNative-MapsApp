541fc240cd10c538cc6f31832794fe57544f7bbb
Mauri <mauri.monzon91@gmail.com>
2024-08-08 14:26:59 -0300

    Revizar permisos

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -1,4 +1,5 @@
 import {
+    check,
   openSettings,
   PERMISSIONS,
   request,
@@ -34,5 +35,30 @@ export const requestLocationPermission =
     };
 
     return permissionMapper[status] ?? 'undetermined';
-    
+
   };
+
+
+  export const checkLocationPermission= async():Promise<PermissionStatus>=>{
+    
+    let status: RNPermissionStatus = 'unavailable';
+
+    if (Platform.OS === 'ios') {
+        status = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      } else if (Platform.OS === 'android') {
+        status = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      } else {
+        /*  return 'unavailable'; */
+        throw new Error('PLataforma no soportada');
+      }
+      
+      const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+        granted: 'granted',
+        denied: 'denied',
+        blocked: 'denied',
+        unavailable: 'unavailable',
+        limited: 'limited',
+      };
+  
+      return permissionMapper[status] ?? 'undetermined';
+  }
