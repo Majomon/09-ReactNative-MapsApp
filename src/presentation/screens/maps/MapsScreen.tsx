a1c04feed1bb495746f7bae4a53aba10098d969f
Mauri <mauri.monzon91@gmail.com>
2024-08-08 18:44:52 -0300

    Acciones de localizacion

--- src/presentation/screens/maps/MapsScreen.tsx
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Map} from '../../components/maps/Map';
+import {getCurrentLocation} from '../../../actions/location/location';
 
 export const MapsScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Map />
-    </View>
-  );
+
+  return <View style={styles.container}>{/*     <Map /> */}</View>;
 };
 
 const styles = StyleSheet.create({
