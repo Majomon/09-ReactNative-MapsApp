9a0900feaf3e98efc0abac64f8c10256bd360b73
Mauri <mauri.monzon91@gmail.com>
2024-08-08 15:16:20 -0300

    Navegacion el con permiso

--- src/presentation/providers/PermissionsCheker.tsx
+++ src/presentation/providers/PermissionsCheker.tsx
@@ -1,9 +1,24 @@
 import React, {PropsWithChildren, useEffect} from 'react';
 import {AppState} from 'react-native';
 import {usePermissionStore} from '../store/permissions/usePermissionStore';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParams} from '../navigation/StackNavigator';
 
 export const PermissionsCheker = ({children}: PropsWithChildren) => {
   const {locationStatus, checkLocationPermission} = usePermissionStore();
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    if (locationStatus === 'granted') {
+      navigation.navigate('MapsScreen');
+    } else if (locationStatus !== 'undetermined') {
+      navigation.navigate('PermissionsScreen');
+    }
+  }, [locationStatus]);
+
+  useEffect(() => {
+    checkLocationPermission();
+  });
 
   useEffect(() => {
     const suscription = AppState.addEventListener('change', nextAppState => {
--- src/presentation/screens/maps/MapsScreen.tsx
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import {Text, View} from 'react-native';
 
 export const MapsScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>MapsScreen</Text>
     </View>
   );
