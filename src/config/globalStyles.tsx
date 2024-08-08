53d073f4cc8bae1da0ee04fae0f03d9bcd1e88ea
Mauri <mauri.monzon91@gmail.com>
2024-08-08 14:53:55 -0300

    Consumiendo store

---
+++ src/config/globalStyles.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  btnPrimarty: {
+    backgroundColor: 'black',
+    paddingVertical: 10,
+    paddingHorizontal:20,
+    borderRadius:100,
+    margin:10
+  },
+});
--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
+import { globalStyles } from '../../../config/globalStyles';
+import { usePermissionStore } from '../../store/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
+  const {locationStatus, requestLocationPermission}= usePermissionStore()
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>PermissionsScreen</Text>
+
+      <Pressable style={globalStyles.btnPrimarty} onPress={requestLocationPermission}>
+        <Text style={{color:"white"}}>Habilitar localización</Text>
+      </Pressable>
+
+      <Text>Estado actual: {locationStatus} </Text>
     </View>
   );
 };
