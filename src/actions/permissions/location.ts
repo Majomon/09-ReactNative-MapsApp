024b188f3b76e65972a4e49067ea9aede15f9c24
Mauri <mauri.monzon91@gmail.com>
2024-08-08 14:23:31 -0300

    Solicitar y revisar permisos

---
+++ src/actions/permissions/location.ts
@@ -0,0 +1,38 @@
+import {
+  openSettings,
+  PERMISSIONS,
+  request,
+  PermissionStatus as RNPermissionStatus,
+} from 'react-native-permissions';
+import type {PermissionStatus} from '../../interfaces/permissions';
+import {Platform} from 'react-native';
+
+export const requestLocationPermission =
+  async (): Promise<PermissionStatus> => {
+    let status: RNPermissionStatus = 'unavailable';
+
+    if (Platform.OS === 'ios') {
+      status = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else if (Platform.OS === 'android') {
+      status = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    } else {
+      /*  return 'unavailable'; */
+      throw new Error('PLataforma no soportada');
+    }
+
+    if (status === 'blocked') {
+      //Abre las settins del dispositivo
+      await openSettings();
+    }
+
+    const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+      granted: 'granted',
+      denied: 'denied',
+      blocked: 'denied',
+      unavailable: 'unavailable',
+      limited: 'limited',
+    };
+
+    return permissionMapper[status] ?? 'undetermined';
+    
+  };
